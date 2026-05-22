f1dae4e74559afddb36bd4862afc29b4a965a80f
tzz <tangbe9@gmail.com>
2026-05-22 09:51:43 +0800

    up

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,16 +6,19 @@ variable "instance_type" {
 variable "ami_id" {
   description = "macOS AMI ID (use a custom AMI with Xcode pre-installed for faster startup)"
   type        = string
+  default = "ami-00dff152c5b051c30" 
 }
 
 variable "availability_zone" {
   description = "AZ that supports Mac instances"
   type        = string
+  default = "us-west-1"
 }
 
 variable "vpc_id" {
   description = "VPC to launch the instance in"
   type        = string
+  default = "vpc-09dc86111eb67d219"
 }
 
 variable "subnet_id" {
@@ -26,7 +29,7 @@ variable "subnet_id" {
 variable "key_pair_name" {
   description = "EC2 key pair name for initial SSH access"
   type        = string
-  default = "todo"
+  default = "tzz-dev"
 }
 
 variable "suspended" {
